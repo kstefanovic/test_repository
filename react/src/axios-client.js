855c8c026a073edc9ba4eb619e3a9fdf3ccb14c1
Kevin Stefanovic <kevin83.stefanovic95@hotmail.com>
2025-03-15 12:46:03 +0800

    init project

---
+++ react/src/axios-client.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useStateContext } from "./context/ContextProvider.jsx";
+
+const axiosClient = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+});
+
+axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('ACCESS_TOKEN');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    try {
+      const { response } = error;
+      const errMsg = response?.data?.message || 'unknown error';
+      console.warn(errMsg);
+      const navigate = useNavigate();
+      const { setNotification } = useStateContext();
+  
+      if (response.status === 401) {
+        localStorage.removeItem('ACCESS_TOKEN');
+        setNotification("Session expired. Please log in again.");
+        navigate("/login");
+      } else if (response.status === 404) {
+        setNotification("Requested resource not found.");
+        navigate("/not-found");
+      } else {
+        setNotification("An error occurred. Please try again.");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+
+    throw error;
+  }
+);
+
+export default axiosClient;
